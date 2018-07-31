67aabeefb1d0bd2cdbd008961375b63e9b5eba6f
Vahe <abelyan.vahe@gmail.com>
2018-07-31 17:51:29 +0400

    Initial commit

---
+++ Setup/InstallData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Yereone\CustomCategoryAttribute\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\InstallDataInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * Installs data for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'custom_category_attr',
+            [
+                'type'         => 'varchar',
+                'label'        => 'Yereone Custom Category Attribute',
+                'input'        => 'text',
+                'sort_order'   => '',
+                'source'       => '',
+                'global'       => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'visible'      => true,
+                'required'     => false,
+                'user_defined' => false,
+                'default'      => null,
+                'group'        => '',
+                'backend'      => ''
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
